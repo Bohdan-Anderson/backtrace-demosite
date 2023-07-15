fddaf77f699a1dd47f5a31ae4a5aeb9c7c40cf0c
Bohdan Anderson <bohdan.anderson@rangle.io>
2023-07-14 23:18:45 -0400

    implemented

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,137 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+type MatrixCell = {value:number,reason:string[]};
+
+function minimumEditDistance(str1:string, str2:string): {minimumDistance: number, matrix: MatrixCell[][]}{
+
+  const m = str1.length;
+  const n = str2.length;
+
+  // Initialize the matrix
+  const matrix = Array(m + 1)
+    .fill(null)
+    .map(() => Array(n + 1).fill({value:0,reason:[]}));
+
+  // Fill the matrix
+  for (let i = 0; i <= m; i++) {
+    for (let j = 0; j <= n; j++) {
+      
+      // basics
+      if (i === 0 && j === 0) {
+        matrix[i][j] = {value:0,reason:[]};
+      } else if (i === 0) {
+        matrix[i][j] = {value:j,reason:[]};
+      } else if (j === 0) {
+        matrix[i][j] = {value:i,reason:[]};
+      } else {
+        // Is it a delete
+        const del = matrix[i][j - 1].value + 1;
+
+        // Is it an insert
+        const ins = matrix[i - 1][j].value + 1;
+
+        // Is it a replace
+        let rep = 0;
+        if(str1[i-1] === str2[j-1]){
+          rep = matrix[i - 1][j - 1].value;
+        } else {
+          rep = matrix[i - 1][j - 1].value + 2;
+        }
+
+        // matrix[i][j] = Math.min(del, ins, rep);
+        matrix[i][j] = {value:Math.min(del, ins, rep),reason:[]};
+        if(matrix[i][j].value === ins) matrix[i][j].reason.push('↑');
+        if(matrix[i][j].value === del) matrix[i][j].reason.push('←');
+        if(matrix[i][j].value === rep) matrix[i][j].reason.push('↖︎');
+      }
+    }
+  }
+
+  // Get the minimum edit distance
+  const minimumDistance = matrix[m][n];
+
+  return {
+    minimumDistance,
+    matrix,
+  };
+}
+
+
+const createTable = (matrix: MatrixCell[][],str1:string, str2:string) => {
+  // matrix to table
+  const table = document.createElement("table");
+  table.classList.add("striped");
+  table.style.border = "1px solid black";
+  table.style.borderCollapse = "collapse";
+
+  // for each letter in str1 add a column
+  const headerRow = document.createElement("tr");
+  const headerCell = document.createElement("th");
+  headerRow.appendChild(headerCell);
+  for (let i = 0; i < str1.length; i++) {
+    const headerCell = document.createElement("th");
+    headerCell.textContent = str1[i];
+    headerRow.appendChild(headerCell);
+  }
+  table.appendChild(headerRow);
+
+
+  for (let i = 0; i < matrix.length; i++) {
+    const row = document.createElement("tr");
+    const headerCell = document.createElement("th");
+    headerCell.textContent = str2[i];
+    row.appendChild(headerCell);
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      const cell = document.createElement("td");
+      cell.innerHTML = matrix[i][j].reason.join(",") + (matrix[i][j].reason.length?'<br>':'') + matrix[i][j].value;
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
+  const tableContainer = document.querySelector<HTMLDivElement>("#table");
+  if(!tableContainer) throw new Error("table container not found");
+  tableContainer.innerHTML = "";
+  tableContainer.appendChild(table);
+}
+
+
+const generate = () => {
+  // Example usage
+  const str1 =(document.getElementById("string1") as HTMLInputElement).value || "execution";
+  const str2 = (document.getElementById("string2") as HTMLInputElement).value || "intention";
+
+  const result = minimumEditDistance(str1, str2);
+  console.log("Minimum Edit Distance:", result.minimumDistance);
+  console.log("Matrix:", result.matrix);
+
+
+  createTable(result.matrix,"#"+str1,"#"+str2);
+}
+
+let button = document.querySelector<HTMLButtonElement>("#generate");
+if(!button) throw new Error("button not found");
+button.addEventListener("click",generate);
+
+
+const defaults = () => {
+  (document.getElementById("string1") as HTMLInputElement).value = "execution";
+  (document.getElementById("string2") as HTMLInputElement).value = "intention";
+  generate();
+}
+button = document.querySelector<HTMLButtonElement>("#button-default");
+if(!button) throw new Error("button not found");
+button.addEventListener("click",defaults);
+
+
+const bohdanExample = () => {
+  (document.getElementById("string1") as HTMLInputElement).value = "bohdan";
+  (document.getElementById("string2") as HTMLInputElement).value = "bo dan";
+  generate();
+}
+button = document.querySelector<HTMLButtonElement>("#button-bohdan");
+if(!button) throw new Error("button not found");
+button.addEventListener("click",bohdanExample);
+
+
